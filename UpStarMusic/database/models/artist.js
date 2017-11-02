c233b79badd687881954894eb4c2c8551d22228b
kazadi <kazadix2@gmail.com>
2017-11-01 21:21:32 -0500

    added artist + album models

--- UpStarMusic/database/models/artist.js
+++ UpStarMusic/database/models/artist.js
@@ -1,1 +1,28 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+const Schema = mongoose.Schema;
+
+
+//Properties of the Artist object
+const ArtistSchema = new Schema({
+	name: {
+		type: String,
+		validate: {
+			validator: (name) => name.length > 2 ,
+			message: 'Name must be longer than 2 characters.'
+		},
+		required: [true, 'Name is required.']
+	},
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    website: String,
+    netWorth: Number,
+    labelName: String,
+    retired: Boolean,
+	albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+module.exports = ArtistSchema;
