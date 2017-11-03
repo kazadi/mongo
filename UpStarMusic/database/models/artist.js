89fbfe7e52cab51088b471a504eeaf44de4c276d
kazadi <kazadix2@gmail.com>
2017-11-02 23:09:03 -0500

    fixed artist model and added findArtist query

--- UpStarMusic/database/models/artist.js
+++ UpStarMusic/database/models/artist.js
@@ -5,14 +5,7 @@ const Schema = mongoose.Schema;
 
 //Properties of the Artist object
 const ArtistSchema = new Schema({
-	name: {
-		type: String,
-		validate: {
-			validator: (name) => name.length > 2 ,
-			message: 'Name must be longer than 2 characters.'
-		},
-		required: [true, 'Name is required.']
-	},
+	name: String,
     age: Number,
     yearsActive: Number,
     image: String,
@@ -25,4 +18,4 @@ const ArtistSchema = new Schema({
 });
 
 const Artist = mongoose.model('artist', ArtistSchema);
-module.exports = ArtistSchema;
+module.exports = Artist;
