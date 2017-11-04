4fef0c437c2926679ab0364925cbbe77bb2d65ab
kazadi <kazadix2@gmail.com>
2017-11-04 11:12:35 -0500

    added min max queries for search

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const minVal = Artist
+    .find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+    const maxVal = Artist
+    .find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+    
+    return Promise.all([minVal, maxVal])
+    .then((result) => {
+        return {min: result[0],max: result[1]}
+    })
 };
